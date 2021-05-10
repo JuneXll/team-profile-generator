4f1c375e229cb0fdcf52715871edae28460347ee
JuneXll <71652307+JuneXll@users.noreply.github.com>
2021-05-10 12:31:25 -0400

    Updated starting files with constructors

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-index.js
+const inquirer=require("inquirer");
+const fs=require("fs");
+
+const Employee=require("./lib/employee");
+const Engineer=require("./lib/engineer");
+const Manager=require("./lib/manager");
+const Intern=require("./lib/intern");
+
+let myTeam=[];
+
+function start(){
+    inquirer.prompt([
+        {
+            type:"input",
+            name:"team"
+        }
+    ])
+}
